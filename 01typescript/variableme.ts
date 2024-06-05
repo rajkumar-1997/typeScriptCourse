113599066a9e4521838dc73b854563be20451fe8
Rajkumar Jangam <rajkumarJangam66@gmail.com>
2024-06-05 15:13:33 +0530

    any

--- 01typescript/variableme.ts
+++ 01typescript/variableme.ts
@@ -1,4 +1,19 @@
+//string
 let str:string='hello raj';
 console.log(str);
+
+//number
+let userId:number=34444.3;
+
+
+//boolean
+let isLoggedIn:boolean=false;
+
+//any(avoid to use any)
+let hero;  
+function getHero(){
+    return "hero";
+}
+hero=getHero();
 
 export {}
