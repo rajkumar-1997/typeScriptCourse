197e08f1710f375f1e92e699d679d259e7e26486
Rajkumar Jangam <rajkumarJangam66@gmail.com>
2024-06-05 15:32:45 +0530

    function started

---
+++ 02function/myfunction.js
@@ -0,0 +1,16 @@
+function addTwo(num) {
+    return num + 2;
+}
+addTwo(4);
+function toUpper(str) {
+    return str.toUpperCase();
+}
+//? is for optional parameter
+var isLogin = function (name, email, isPaid) {
+};
+isLogin('dg', 'fd');
+var isSignup = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+isSignup('sff', 'fsf');
+toUpper('raj');
---
+++ 02function/myfunction.ts
@@ -0,0 +1,23 @@
+function addTwo(num:number){
+    return num+2;
+}
+addTwo(4);
+
+function toUpper(str:string){
+    return str.toUpperCase();
+}
+
+
+//? is for optional parameter
+const isLogin=(name:string,email:string,isPaid?:boolean)=>{
+
+}
+isLogin('dg','fd');
+
+//providing default value for isPaid
+const isSignup=(name:string,email:string,isPaid:boolean=false)=>{
+
+}
+isSignup('sff','fsf');
+
+toUpper('raj');
