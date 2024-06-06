7957abe4847891e45a232e17c220baf5636f8c3b
Rajkumar Jangam <rajkumarJangam66@gmail.com>
2024-06-06 15:53:24 +0530

    function done

--- 02function/myfunction.ts
+++ 02function/myfunction.ts
@@ -1,4 +1,4 @@
-function addTwo(num:number){
+const addTwo=(num:number):number=>{
     return num+2;
 }
 addTwo(4);
@@ -21,3 +21,21 @@ const isSignup=(name:string,email:string,isPaid:boolean=false)=>{
 isSignup('sff','fsf');
 
 toUpper('raj');
+
+const heros=['thor','spiderman','ironman'];
+
+heros.map((hero:string):string=>{
+    return `good ${hero}`;
+})
+
+
+//not returning anything will give void
+function consoleError(message:string):void{
+ console.log(message);
+}
+
+
+// The never type represents values which are never observed. In a return type, this means that the function throws an exception or terminates execution of the program.
+function fail(msg: string): never {
+    throw new Error(msg);
+  }
