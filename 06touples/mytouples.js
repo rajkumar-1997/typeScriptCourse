d4a5da6a00edeee20772a198747eb4d9b9c67553
Rajkumar Jangam <rajkumarJangam66@gmail.com>
2024-06-13 21:08:38 +0530

    touples done

---
+++ 06touples/mytouples.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var arr = [1, 'fdgd', true];
+var de = [1, 'dg'];
---
+++ 06touples/mytouples.ts
@@ -0,0 +1,11 @@
+const arr:[number,string,boolean]=[1,'fdgd',true];
+
+// const newarr:[string,number]=[1,'fdfd']  this will give error
+
+type User=[number,string]
+const de:User=[1,'dg'];
+
+
+
+
+export {}
